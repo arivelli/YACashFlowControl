bf825119352578927505f198f6a8971d70b15898
Mariana Coloccini <mariana@Maru.local>
2020-03-20 15:53:38 -0300

    m

--- public/js/cashFlow.js
+++ public/js/cashFlow.js
@@ -4,18 +4,18 @@ function filterData() {
 
     window.cashFlow.settlement_date = filter.settlementDate;
 
-    if(typeof window.cashFlow[filter.settlementDate] !== 'undefined'){
+    if (typeof window.cashFlow[filter.settlementDate] !== 'undefined') {
         filterData2(filter);
     } else {
-        $.ajax( "/cashFlowData/" + filter.settlementDate )
-        .done(function(res) {
-            window.cashFlow[filter.settlementDate] = [];
-            window.cashFlow[filter.settlementDate] = res;
-            filterData2(filter);
-        })
-        .fail(function() {
-            alert( "error" );
-        });
+        $.ajax("/cashFlowData/" + filter.settlementDate)
+            .done(function(res) {
+                window.cashFlow[filter.settlementDate] = [];
+                window.cashFlow[filter.settlementDate] = res;
+                filterData2(filter);
+            })
+            .fail(function() {
+                alert("error");
+            });
     }
 }
 
@@ -23,7 +23,7 @@ function filterData2(newFilter) {
 
     //Clone the cashflow table
     data = JSON.parse(JSON.stringify(window.cashFlow[newFilter.settlementDate]))
-    //Add the entryType filter only in case the amount of options selected is less than the total
+        //Add the entryType filter only in case the amount of options selected is less than the total
     if (newFilter.entryType.length !== 5) {
         data = data.filter(function(entry) {
             return (newFilter.entryType.indexOf(entry.entry_type) > -1);
@@ -41,33 +41,33 @@ function filterData2(newFilter) {
     }
 
     let groupedData = {};
-    if(newFilter.view != 'cashFlow') {
+    if (newFilter.view != 'cashFlow') {
         let groupByFunction = groupBy(newFilter.view);
         groupedData = groupByFunction(data);
     }
-    
+
     let html = '';
     let links = [];
     dataKeys = Object.keys(groupedData);
-    
+
     dataKeys.forEach((dataKey) => {
         let caption = dataKey.slice(0);
-        if( newFilter.view  === 'entry_type' ){
+        if (newFilter.view === 'entry_type') {
             caption = getEntryType(caption);
         }
-        if( newFilter.view  === 'settlement_week' ){
+        if (newFilter.view === 'settlement_week') {
             caption = getOrdinalNumber(caption);
         }
         links.push('<a href="#' + caption + '">' + caption + '</a>');
         html += drawTable(groupedData[dataKey], caption);
-        
+
 
     });
 
     $('.filterSubtitle').html(views[newFilter.view] + links.join(' | '))
-    
+
     $('#tables_group').html(html);
-    $('.'+newFilter.view).hide();
+    $('.' + newFilter.view).hide();
 }
 
 const groupBy = key => array =>
@@ -78,7 +78,7 @@ const groupBy = key => array =>
     }, {});
 
 
-function showModalExecuteOperation(id){
+function showModalExecuteOperation(id) {
     let operation = window.cashFlow[window.cashFlow.settlement_date].find((e) => { return e.operation_id == id });
     $('#cancel-operation_concept').html(operation.concept);
     $('#cancel-operation_detail').html(operation.detail);
@@ -87,38 +87,39 @@ function showModalExecuteOperation(id){
     $('#cancel-operation_operation_id').val(operation.operation_id);
     $('#cancel-operation_operation_date').val(operation.estimated_date);
     set_dollar_value();
-    
+
     $('#cancel-operation_operation_amount').priceFormat({
-        'prefix' : operation.currency + " ",
-        'centsSeparator' : ",",
-        'thousandsSeparator' : ".",
-        'centsLimit'  : 2
+        'prefix': operation.currency + " ",
+        'centsSeparator': ",",
+        'thousandsSeparator': ".",
+        'centsLimit': 2
     });
     $('#modal-datamodal-cancel-operation').modal('show');
 }
 
-function executeOperation(id){
+function executeOperation(id) {
     let data = {};
     var numberPattern = /\d+/g;
 
-    data.operation_amount = $('#cancel-operation_operation_amount').val().match( numberPattern ).join('');
-    data.dollar_value = $('#cancel-operation_dollar_value').val().match( numberPattern ).join('');
+    data.operation_amount = $('#cancel-operation_operation_amount').val().match(numberPattern).join('');
+    data.dollar_value = $('#cancel-operation_dollar_value').val().match(numberPattern).join('');
     data.account_id = $('#cancel-operation_account_id').val();
     data.operation_id = $('#cancel-operation_operation_id').val();
     data.operation_date = $('#cancel-operation_operation_date').val();
 
 
-    $.post( "/admin/app_operations/execute/" + id , data)
+    $.post("/admin/app_operations/execute/" + id, data)
         .done(function(res) {
             alert("ok");
         })
         .fail(function() {
-            alert( "error" );
+            alert("error");
         });
-    
+
 }
+
 function drawTable(table, caption) {
-    
+
     let total_estimated_amount = 0;
     let total_operation_amount = 0;
     let html = `
@@ -137,9 +138,9 @@ function drawTable(table, caption) {
                 <th class="account_name"><a href="">Cuenta</a></th>
                 <th class="area"><a href="">Area</a></th>
                 <th class="category"><a href="">Categoría</a></th>
-                <th style="width:60px;text-align:right;"><a href="">Monto estimado</a></th>
-                <th style="width:60px;text-align:right;"><a href="">Monto operación</a></th>
-                <th style="width:60px">Acciones</th>
+                <th style="width:100px;text-align:right;"><a href="">Monto estimado</a></th>
+                <th style="width:100px;text-align:right;"><a href="">Monto operación</a></th>
+                <th style="width:100px">Acciones</th>
             </tr>
         </thead>
         <tbody>`;
@@ -158,7 +159,7 @@ function drawTable(table, caption) {
             <td class="right">` + moneyFormat(row.estimated_amount, row.currency) + `</td>
             <td class="right">` + moneyFormat(row.operation_amount, row.currency) + `</td>
             <td class="right">`;
-        if(row.is_done != 1) {
+        if (row.is_done != 1) {
             html += `
             <a href="javascript: void showModalExecuteOperation(` + row.operation_id + `)" class="btn btn-success btn-xs"><i class="fa fa-money"></i></a>`;
         }
@@ -212,30 +213,29 @@ function moneyFormat(number, currency) {
         let newNumber = [];
         number = number + '';
         str = number.split("").reverse();
-        str.forEach((n,i)=>{
+        str.forEach((n, i) => {
             if (i === 2) {
                 newNumber.push(',');
-            } else if ( ( i - 2 ) / 3 == Math.round( (i - 2) / 3 ) && i > 3 ) {
+            } else if ((i - 2) / 3 == Math.round((i - 2) / 3) && i > 3) {
                 newNumber.push('.');
             }
             newNumber.push(n);
         });
         currency = (typeof currency != 'undefined') ? currency : '';
-        res =  currency + ' ' + newNumber.reverse().join("");
+        res = currency + ' ' + newNumber.reverse().join("");
     }
     return res;
 }
 
 function dateFormat(date) {
-    if (typeof date === 'undefined' || date == '' || date == null){
+    if (typeof date === 'undefined' || date == '' || date == null) {
         return '&nbsp;';
     }
-    dateParts = date.substring(0,10).split('-');
+    dateParts = date.substring(0, 10).split('-');
     return dateParts[2] + '-' + dateParts[1] + '-' + dateParts[0];
 }
 
-function getEntryType(type)
-{
+function getEntryType(type) {
     type = parseInt(type);
     let res = '';
     switch (type) {
@@ -257,13 +257,13 @@ function getEntryType(type)
     }
     return res;
 }
-function getOrdinalNumber(number, genre)
-{
+
+function getOrdinalNumber(number, genre) {
     number = parseInt(number);
     genre = (typeof genre !== 'undefined') ? genre : 'female';
-    let maleNumbers = ['-','Primero','Segundo','Tercero','Cuarto','Quinto'];
-    let femaleNumbers = ['-','Primera','Segunda','Tercera','Cuarta','Quinta'];
-    if(genre === 'male'){
+    let maleNumbers = ['-', 'Primero', 'Segundo', 'Tercero', 'Cuarto', 'Quinto'];
+    let femaleNumbers = ['-', 'Primera', 'Segunda', 'Tercera', 'Cuarta', 'Quinta'];
+    if (genre === 'male') {
         res = maleNumbers[number];
     } else {
         res = femaleNumbers[number];
@@ -271,17 +271,17 @@ function getOrdinalNumber(number, genre)
     return res;
 }
 
-function set_dollar_value(){
-    if (typeof window.cashFlow.dollar_value === 'undefined'){
+function set_dollar_value() {
+    if (typeof window.cashFlow.dollar_value === 'undefined') {
         window.cashFlow.dollar_value = {};
     }
     let quote_date = $('#cancel-operation_operation_date').val();
-    if(typeof window.cashFlow.dollar_value[quote_date] !== 'undefined') {
+    if (typeof window.cashFlow.dollar_value[quote_date] !== 'undefined') {
         $('#cancel-operation_dollar_value').val(window.cashFlow.dollar_value[quote_date]);
         $('#cancel-operation_dollar_value').trigger('keyup');
     } else {
         $.get({
-            url : '/dollarValue/getvalueof/'+quote_date,
+            url: '/dollarValue/getvalueof/' + quote_date,
             success: (data) => {
                 $('#cancel-operation_dollar_value').val(data);
                 window.cashFlow.dollar_value[quote_date] = data;
@@ -296,14 +296,14 @@ function runFilter(event) {
     let filter = window.cashFlow.filter;
     let elem = event.currentTarget;
 
-    if(elem.name === 'year' || elem.name === 'month' || elem.name === 'view'){
+    if (elem.name === 'year' || elem.name === 'month' || elem.name === 'view') {
         filter[elem.name] = elem.value;
     } else {
         let value = elem.name === 'entryType' ? parseInt(elem.value) : elem.value;
-        if(filter[elem.name].indexOf( value ) === -1) {
-            filter[elem.name].push( value );
+        if (filter[elem.name].indexOf(value) === -1) {
+            filter[elem.name].push(value);
         } else {
-            filter[elem.name] = filter[elem.name].filter(function(values, index, arr){ return values !== value;});
+            filter[elem.name] = filter[elem.name].filter(function(values, index, arr) { return values !== value; });
         }
     }
     filter['settlementDate'] = filter.year + filter.month;
@@ -311,31 +311,31 @@ function runFilter(event) {
     filterData();
 }
 
-function drawFilter(){
+function drawFilter() {
     let filter = window.cashFlow.filter;
 
     //Remove all class for click
     $('#filterForm button').removeClass('btn-info').addClass('btn-default');
     $('button[name="view"]').removeClass('btn-danger');
-    
+
     //Add click class
     $('button[name="year"][value=' + filter.year + ']').addClass('btn-info').removeClass('btn-default');
 
     $('button[name="month"][value=' + filter.month + ']').addClass('btn-info').removeClass('btn-default');
 
-    filter.entryType.forEach((eT)=>{
+    filter.entryType.forEach((eT) => {
         $('button[name="entryType"][value=' + eT + ']').addClass('btn-info').removeClass('btn-default');
     })
-    filter.status.forEach((s)=>{
+    filter.status.forEach((s) => {
         $('button[name="status"][value=' + s + ']').addClass('btn-info').removeClass('btn-default');
     })
     $('button[name="view"][value=' + filter.view + ']').addClass('btn-danger').removeClass('btn-default');
 
     $('.filterTitle').html(months[filter.month] + ' ' + filter.year);
-    
+
 }
 
-$(function () {
+$(function() {
     $('.filterField').on('click', (e) => {
         runFilter(e);
     });
@@ -343,24 +343,24 @@ $(function () {
 });
 
 const months = {
-    '01' : 'ENERO',
-    '02' : 'FEBRERO',
-    '03' : 'MARZO',
-    '04' : 'ABRIL',
-    '05' : 'MAYO',
-    '06' : 'JUNIO',
-    '07' : 'JULIO',
-    '08' : 'AGOSTO',
-    '09' : 'SEPTIEMBRE',
-    '10' : 'OCTUBRE',
-    '11' : 'NOVIEMBRE',
-    '12' : 'DICIEMBRE'
+    '01': 'ENERO',
+    '02': 'FEBRERO',
+    '03': 'MARZO',
+    '04': 'ABRIL',
+    '05': 'MAYO',
+    '06': 'JUNIO',
+    '07': 'JULIO',
+    '08': 'AGOSTO',
+    '09': 'SEPTIEMBRE',
+    '10': 'OCTUBRE',
+    '11': 'NOVIEMBRE',
+    '12': 'DICIEMBRE'
 }
 
 const views = {
-    'settlement_week' : 'Semana: ',
-    'entry_type' : 'Tipo: ',
-    'account_name' : 'Cuenta: ',
-    'area' : 'Area: ',
-    'category' : 'Categoría: '
+    'settlement_week': 'Semana: ',
+    'entry_type': 'Tipo: ',
+    'account_name': 'Cuenta: ',
+    'area': 'Area: ',
+    'category': 'Categoría: '
 }
