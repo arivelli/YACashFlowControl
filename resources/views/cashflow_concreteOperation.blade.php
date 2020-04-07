42a94fa6dd86b86136b74d5091b5d21954bd8ad3
Mariana Coloccini <mariana@Maru.local>
2020-04-07 20:07:28 -0300

    Prueba 8

--- resources/views/cashflow_concreteOperation.blade.php
+++ resources/views/cashflow_concreteOperation.blade.php
@@ -110,9 +110,18 @@
                     </div>
                 </div>
 
-                <input type="hidden" id="cancel-operation_operation_id" value="">
+                <div class="form-group">
+                    <div class="col-sm-12" style="padding: 5px 0 !important">
+                        <label class="control-label col-sm-3">Notas</label>
+                        <div class="input-group col-sm-9"><input type="hidden" id="cancel-operation_operation_id" value="">
                 <button class='btn btn-sm btn-default' class="close" data-dismiss="modal" >Cancelar</button>
-                <button class='btn btn-sm btn-success' onclick="executeOperation( $('#cancel-operation_operation_id').val() ); return false">Concretar</button>
+                <button class='btn btn-sm btn-success' onclick="executeOperation( $('#cancel-operation_operation_id').val() ); return false">Concretar</button></div>
+                    </div>
+                </div>
+
+
+
+                
               </div>
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
