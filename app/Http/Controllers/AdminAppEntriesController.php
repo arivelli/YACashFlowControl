0fb87a69556b6f5450266eeb78e7c9ad9f1b9f9c
Mariana Coloccini <mariana@Maru.local>
2020-04-08 16:44:15 -0300

    prueba9

--- app/Http/Controllers/AdminAppEntriesController.php
+++ app/Http/Controllers/AdminAppEntriesController.php
@@ -561,7 +561,11 @@ class AdminAppEntriesController extends \arivelli\crudbooster\controllers\CBCont
 
 		$operations = GenerateOperationsFromPlan::compute_operations($plan);
 		$html = '<table width="100%" style="border-top: solid 1px #E9E9E9"">';
-
+			$html .= '<tr style="border-bottom: solid 1px #E9E9E9; margin-top:5px">';
+			$html .= '<th style="padding:5px">Detalle</td>';
+			$html .= '<th style="padding:5px">Fecha</td>';
+			$html .= '<th style="padding:5px">Monto</td>';
+			$html .= '</tr>';
 		foreach ($operations as $operation) {
 			$html .= '<tr style="border-bottom: solid 1px #E9E9E9; margin-top:5px">';
 			$html .= "<td style='padding:5px'>{$operation['detail']}</td>";
